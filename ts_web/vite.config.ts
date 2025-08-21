8ba41f079374c9ea6723a4ff29a44bb5e9a4548c
treefrog <110452+treefrog00@users.noreply.github.com>
2025-08-21 14:55:27 +0100

    allow running dist version from opened file without need for dev server, for whatever reason

--- ts_web/vite.config.ts
+++ ts_web/vite.config.ts
@@ -1,5 +1,25 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // No special config needed for a simple web app
+  build: {
+    rollupOptions: {
+      output: {
+        format: 'iife',
+        entryFileNames: 'app.js',
+        chunkFileNames: 'app.js',
+        assetFileNames: 'app.[ext]'
+      }
+    }
+  },
+  plugins: [
+    {
+      name: 'html-transform',
+      transformIndexHtml(html) {
+        return html
+          .replace(/crossorigin/g, '')
+          .replace(/type="module"/g, '')
+          .replace(/src="\/[^"]*"/g, 'src="./app.js"');
+      }
+    }
+  ]
 })
