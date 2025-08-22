82ff93b9c10001210148b7d9f42f946833634cd7
treefrog <110452+treefrog00@users.noreply.github.com>
2025-08-22 19:41:30 +0100

    Revert "allow running dist version from opened file without need for dev server, for whatever reason"
    This reverts commit 8ba41f079374c9ea6723a4ff29a44bb5e9a4548c.

--- ts_web/vite.config.ts
+++ ts_web/vite.config.ts
@@ -1,25 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      output: {
-        format: 'iife',
-        entryFileNames: 'app.js',
-        chunkFileNames: 'app.js',
-        assetFileNames: 'app.[ext]'
-      }
-    }
-  },
-  plugins: [
-    {
-      name: 'html-transform',
-      transformIndexHtml(html) {
-        return html
-          .replace(/crossorigin/g, '')
-          .replace(/type="module"/g, '')
-          .replace(/src="\/[^"]*"/g, 'src="./app.js"');
-      }
-    }
-  ]
+  // No special config needed for a simple web app
 })
